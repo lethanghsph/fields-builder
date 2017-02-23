ee3a45ec17e45377c7ece876611cda8533f9a366
LeThang <lethanghsph@gmail.com>
2017-02-24 00:39:54 +0700

    Add radio field

---
+++ fields/tl_field_radio.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This class to build element text field
+ *
+ * @package tl-framework
+ */
+
+/**
+ * Strategy Pattern.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+class TL_Field_Radio extends TL_Field_Set_Output_Abstract {
+	/**
+	 * Instance of object to build field.
+	 *
+	 * @var [object]
+	 */
+	protected $field;
+
+	/**
+	 * Initialize function.
+	 *
+	 * @param TL_Field $tl_field [Class to set field attributes].
+	 */
+	public function __construct( TL_Field $tl_field ) {
+		$this->field = $tl_field;
+	}
+
+	/**
+	 * Set output of field.
+	 */
+	public function set_output() {
+		$field = $this->field;
+		return '<input type="radio" name="' . $field->set_name() . '" value="' . $field->set_value() . '" ' . $field->set_attributes() . '>';
+	}
+}
+<input type="radio" name="gender" value="male" checked> Male<br>
