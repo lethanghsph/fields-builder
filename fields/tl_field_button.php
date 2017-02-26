217eace22f831a87f267e0910deae469b4e8fd1d
LeThang <lethanghsph@gmail.com>
2017-02-26 17:33:57 +0700

    add button

---
+++ fields/tl_field_button.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This class to build element button field
+ *
+ * @package tl-framework
+ */
+
+/**
+ * Strategy Pattern.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+class TL_Field_Button extends TL_Field_Set_Output_Abstract {
+	/**
+	 * Instance of object to build field.
+	 *
+	 * @var [object]
+	 */
+	protected $field;
+
+	/**
+	 * Initialize function.
+	 *
+	 * @param TL_Field $tl_field [Class to set field attributes].
+	 */
+	public function __construct( TL_Field $tl_field ) {
+		$this->field = $tl_field;
+	}
+
+	/**
+	 * Set output of field.
+	 */
+	public function set_output() {
+		$field = $this->field;
+		$ex_atts = array(
+			'class' => 'btn btn-info',
+		);
+		return $this->field->generate_input( array(
+			'atts' => $field->generate_attributes( $ex_atts ),
+		) );
+	}
+}
